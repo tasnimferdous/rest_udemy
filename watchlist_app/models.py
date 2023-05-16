9e51ec9c6658bf695bc6c3631036f43ce859eedf
tasnimferdous <tasnimferdous216@gmail.com>
2023-05-16 22:36:27 +0600

    Function based api_view

---
+++ watchlist_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
