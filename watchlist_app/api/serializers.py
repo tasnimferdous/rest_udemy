668b102a3547563bc9d28d64177775c5e6f9af10
tasnimferdous <tasnimferdous216@gmail.com>
2023-05-17 19:09:25 +0600

    validation and Modelserializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,18 +1,55 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField(required=False, allow_blank=True)
-    active = serializers.BooleanField()
+# validators
+def validate_description(value):
+    if len(value)>100:
+        raise serializers.ValidationError('Description is too long!')
+    return value
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    # object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description cannot be the same!')
+        return data
+
+    # field level validation
+    def validate_name(self, value):
+        if len(value)<3:
+            raise serializers.ValidationError('Name is too short!')
+        return value
+
+
+# Scratch Serializer
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField(required=False, allow_blank=True, validators=[validate_description])
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+
+#     # object level validation
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('Name and description cannot be the same!')
+#         return data
+
+#     # field level validation
+#     def validate_name(self, value):
+#         if len(value)<3:
+#             raise serializers.ValidationError('Name is too short!')
+#         return value
--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from watchlist_app.api import views
 
 urlpatterns = [
-    path('list/', views.movie_list, name = 'movie-list'),
-    path('<int:pk>/', views.movie_details, name = 'movie-details'),
+    # path('list/', views.movie_list, name = 'movie-list'),
+    # path('<int:pk>/', views.movie_details, name = 'movie-details'),
+    path('list/', views.MovieListAV.as_view(), name = 'movie-list'),
+    path('<int:pk>/', views.MovieDetailAV.as_view(), name = 'movie-details'),
 ]
